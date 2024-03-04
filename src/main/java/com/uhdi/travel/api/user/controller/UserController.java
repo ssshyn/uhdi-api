128a2268c451e29f618a5f0d7bd4973c59fa4845
ssshyn <ssshyn.dev@gmail.com>
2024-03-04 15:04:06 +0900

    directory updated

---
+++ src/main/java/com/uhdi/travel/api/user/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.uhdi.travel.api.user.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+    
+}
